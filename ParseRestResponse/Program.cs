c75c6fd85bb1d8627bc6a0f5eecd02bfafdaf8ad
Fred Peters <fred@fredwebs.com>
2016-11-23 18:39:27 -0500

    Working on route display.

--- ParseRestResponse/Program.cs
+++ ParseRestResponse/Program.cs
@@ -1,25 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
+using System.Web;
 using BingMapsRESTService.Common.JSON;
-using Newtonsoft.Json;
 using System.Runtime.Serialization.Json;
 
-// Original from https://msdn.microsoft.com/en-us/library/hh674188.aspx
+// Original program from https://msdn.microsoft.com/en-us/library/hh674188.aspx
 
 namespace ParseRestResponse
 {
     class Program
     {
         private static string _bingMapsKey = string.Empty;
+        // Create Web Api client just once to avoid running out of ports
         private static readonly HttpClient Client = new HttpClient();
-        //private static readonly string BingMapsKey = ApplicationSettingsManager.GetBingMapKey();
-        private const string UrlLocation = "http://dev.virtualearth.net/REST/v1/Locations?query=";
+        private const string BingRestLocation = "http://dev.virtualearth.net/REST/v1/";
 
 
         static void Main(string[] args)
@@ -27,145 +24,162 @@ namespace ParseRestResponse
             try
             {
                 _bingMapsKey = Environment.GetEnvironmentVariable("BingMapsKey");
-                string locationsRequest = CreateRequest("New%20York");
-//                Response locationsResponse = MakeRequest(locationsRequest);
-                Response locationsResponse = MakeRequestWebApi(locationsRequest);
-                ProcessResponse(locationsResponse);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                Console.Read();
-            }
 
-        }
+                // First search for a general location.  We could also put a street address here also.
+                const string locationRequestStr = "New York";
 
-        //Create the request URL
-        public static string CreateRequest(string queryString)
-        {
-            string urlRequest = "http://dev.virtualearth.net/REST/v1/Locations/" +
-                                           queryString +
-                                           "?key=" + _bingMapsKey;
-            return (urlRequest);
-        }
+                var place = WebUtility.UrlEncode(locationRequestStr);
 
-        
-        List<Address> _foundAddresses = new List<Address>();
+                var urlRequest = $"{BingRestLocation}Locations/{place}?key={_bingMapsKey}";
 
-        private static T DeserializeJson<T>(string json)
-        {
-            var instance = typeof(T);
-            var lst = new List<Address>();
+                var locationsResponse = MakeRequestWebApi(urlRequest);
 
-            using (var ms = new MemoryStream(Encoding.Unicode.GetBytes(json)))
-            {
-                DataContractJsonSerializer deserializer = new DataContractJsonSerializer(typeof(T));
-                return (T)deserializer.ReadObject(ms);
-            }
-        }
-        /*
-            someJsonArrayString = "[{\"ID\":7},{\"ID\":16}]";
-            filesToMove = deserializeJSON<List<SomeDataClass>>(someJsonArrayString);
-            Console.WriteLine(filesToMove[1].ID); // returns 16
-             
-        */
-
-        public static Response MakeRequestWebApi(string requestUrl)
-        {
-            //            var url = $"{UrlLocation}{address}&key={BingMapsKey}";
-
-            HttpResponseMessage response = Client.GetAsync(requestUrl).Result;
-
-            if (response.IsSuccessStatusCode)
-            {
-                var jsonString = response.Content.ReadAsStringAsync().Result;
-                //var bingResponse = JsonConvert.DeserializeObject<Response>(jsonString);
-                //return bingResponse;
-
-                //byte[] bytes = Encoding.UTF8.GetBytes(response.Content.ToString());
-                //var locsStream = new MemoryStream(bytes);
-                //locsStream.Position = 0;
-
-
-                //var jasonSerializer = new DataContractJsonSerializer(typeof(Response));
-                //var locsResponse = (Response)jasonSerializer.ReadObject(locsStream);
-
-                var mapResponse = DeserializeJson<Response>(jsonString);
-
-
-
-                return mapResponse;
-
-            }
-
-            return null;
-        }
-
-        public static Response MakeRequest(string requestUrl)
-        {
-            try
-            {
-                HttpWebRequest request = WebRequest.Create(requestUrl) as HttpWebRequest;
-                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                if (locationsResponse.Item2 != "success")
                 {
-                    if (response.StatusCode != HttpStatusCode.OK)
-                        throw new Exception(String.Format(
-                        "Server error (HTTP {0}: {1}).",
-                        response.StatusCode,
-                        response.StatusDescription));
-                    DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(Response));
-                    object objResponse = jsonSerializer.ReadObject(response.GetResponseStream());
-                    Response jsonResponse
-                    = objResponse as Response;
-                    return jsonResponse;
+                    Console.WriteLine($"Bing Maps Request fail, message: {locationsResponse.Item2}");
                 }
+                else
+                {
+                    ProcessLocationResponse(locationsResponse.Item1);
+                }
+
+                // Now find a route, first geocode two locations
+
+                // First location
+
+                const string brillBuildingAddress = "1619 Broadway, New York, NY 10019";
+
+                place = WebUtility.UrlEncode(brillBuildingAddress);
+
+                urlRequest = $"{BingRestLocation}Locations/{place}?key={_bingMapsKey}";
+
+                var fromLocationResponse = MakeRequestWebApi(urlRequest);
+
+                if (locationsResponse.Item2 != "success")
+                {
+                    Console.WriteLine($"Bing Maps Location Request fail, message: {locationsResponse.Item2}");
+                    Console.WriteLine("Press any key to exit");
+                    Console.ReadKey();
+                    return;
+                }
+                var resouresSet = fromLocationResponse.Item1.ResourceSets[0];
+                var locationResource = (Location)resouresSet.Resources[loc];
+
+                var brillLocation = (Location) fromLocationResponse.Item1;
+                // Second location
+
+                const string staxStudiosAddress = "926 E McLemore Ave, Memphis, TN 38126";
+
+                place = WebUtility.UrlEncode(brillBuildingAddress);
+
+                urlRequest = $"{BingRestLocation}Locations/{place}?key={_bingMapsKey}";
+
+                var toLocationResponse = MakeRequestWebApi(urlRequest);
+
+                if (locationsResponse.Item2 != "success")
+                {
+                    Console.WriteLine($"Bing Maps Location Request fail, message: {locationsResponse.Item2}");
+                    Console.WriteLine("Press any key to exit");
+                    Console.ReadKey();
+                    return;
+                }
+
+                var routeUrl = $"{BingRestLocation}Routes?wp.0={fromLocationResponse.Item1.Coordinates.Latitude},{fromLocation.Coordinates.Longitude}&wp.1={toLocation.Coordinates.Latitude},{toLocation.Coordinates.Longitude}&du=mile&key={BingMapsKey}";
+
+                var bingResponse = MakeRequestWebApi(routeUrl);
+
+                if (locationsResponse.Item2 != "success")
+                {
+                    Console.WriteLine($"Bing Maps Location Request fail, message: {locationsResponse.Item2}");
+                }
+                else
+                {
+                    ProcessLocationResponse(locationsResponse.Item1);
+                }
+
+
+                urlRequest = $"{BingRestLocation}/Routes{place}?key={_bingMapsKey}";
+                var routesResponse = MakeRequestWebApi(urlRequest);
+
+                if (routesResponse.Item2 != "success")
+                {
+                    Console.WriteLine($"Bing Maps Route Request fail, message: {locationsResponse.Item2}");
+                }
+                else
+                {
+                    ProcessLocationResponse(locationsResponse.Item1);
+                }
+
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
-                return null;
             }
 
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
 
-        static public void ProcessResponse(Response locationsResponse)
+        /// <summary>
+        /// Makes Web API call to the request URL and returns a Bing Response Object 
+        /// </summary>
+        /// <param name="requestUrl"></param>
+        /// <returns>Bing Response Object which must be cast to one of these objects:
+        ///      Location, Route, Traffic Incident, CompressedPointList, ElevationData, SeaLevelData
+        /// </returns>
+        public static Tuple<Response, string> MakeRequestWebApi(string requestUrl)
         {
+            var httpResponseMessage = Client.GetAsync(requestUrl).Result;
 
-            int locNum = locationsResponse.ResourceSets[0].Resources.Length;
-
-            //Get formatted addresses: Option 1
-            //Get all locations in the response and then extract the formatted address for each location
-            Console.WriteLine("Show all formatted addresses");
-            for (int i = 0; i < locNum; i++)
+            if (!httpResponseMessage.IsSuccessStatusCode)
             {
+                return new Tuple<Response, string>(null, $"Response Status: {httpResponseMessage.StatusCode}");
+            }
 
-//                Location location = locationsResponse.ResourceSets[0].Resources[i];
-//                Console.WriteLine(location.Address.FormattedAddress);
+            var jsonString = httpResponseMessage.Content.ReadAsStringAsync().Result;
 
-                var resouresSets = (ResourceSet) locationsResponse.ResourceSets[0];
-                var resource = (Location) resouresSets.Resources[i];
-                var addr = resource.Address.FormattedAddress;
-                var x = 2;
+            using (var ms = new MemoryStream(Encoding.Unicode.GetBytes(jsonString)))
+            {
+                var deserializer = new DataContractJsonSerializer(typeof(Response));
+                return new Tuple<Response, string>((Response)deserializer.ReadObject(ms), "success");
+            }
+        }
+
+        public static void ProcessLocationResponse(Response locationsResponse)
+        {
+            var locsCount = locationsResponse.ResourceSets[0].Resources.Length;
+
+            // Get all locations in the response and then extract the formatted address for each location
+            Console.WriteLine("Show all formatted addresses");
+
+            var resouresSet = locationsResponse.ResourceSets[0];
+
+            for (var loc = 0; loc < locsCount; loc++)
+            {
+                var locationResource = (Location) resouresSet.Resources[loc];
+                var addr = locationResource.Address.FormattedAddress;
                 Console.WriteLine(addr);
             }
             Console.WriteLine();
 
-
-            //Get the Geocode Points for each Location
-            for (int i = 0; i < locNum; i++)
+            // Get the Geocode Points for each Location
+            for (var loc = 0; loc < locsCount; loc++)
             {
-                Location location = (Location)locationsResponse.ResourceSets[0].Resources[i];
-                Console.WriteLine("Geocode Points for " + location.Address.FormattedAddress);
-                int geocodePointNum = location.GeocodePoints.Length;
-                for (int j = 0; j < geocodePointNum; j++)
+                var locationResource = (Location) resouresSet.Resources[loc];
+                Console.WriteLine("Geocode Points for " + locationResource.Address.FormattedAddress);
+
+                var countGeocodePoints = locationResource.GeocodePoints.Length;
+
+                for (var point = 0; point < countGeocodePoints; point++)
                 {
-                    Console.WriteLine("    Point: " + location.GeocodePoints[j].Coordinates[0].ToString() + "," +
-                                                 location.GeocodePoints[j].Coordinates[1].ToString());
-                    double test = location.GeocodePoints[j].Coordinates[1];
-                    Console.Write("    Usage: ");
-                    for (int k = 0; k < location.GeocodePoints[j].UsageTypes.Length; k++)
+                    var pointInfo = locationResource.GeocodePoints[point];
+
+                    Console.WriteLine($"    Point: {pointInfo.Coordinates[0]}, {pointInfo.Coordinates[1]}");
+
+                    Console.Write("    Usages: ");
+                    for (var usage = 0; usage < pointInfo.UsageTypes.Length; usage++)
                     {
-                        Console.Write(location.GeocodePoints[j].UsageTypes[k].ToString() + " ");
+                        Console.Write($"{pointInfo.UsageTypes[usage]} ");
                     }
                     Console.WriteLine("\n\n");
                 }
@@ -173,20 +187,73 @@ namespace ParseRestResponse
             Console.WriteLine();
 
 
-            //Get all locations that have a MatchCode=Good and Confidence=High
-            Console.WriteLine("Locations that have a Confidence=High");
-            for (int i = 0; i < locNum; i++)
+            // Get all locations that have a Confidence=High
+            Console.WriteLine("Locations that have Confidence=High");
+
+            for (var loc = 0; loc < locsCount; loc++)
             {
-                Location location = (Location)locationsResponse.ResourceSets[0].Resources[i];
-                if (location.Confidence == "High")
-                    Console.WriteLine(location.Address.FormattedAddress);
+                var locationResource = (Location)resouresSet.Resources[loc];
+
+                if (locationResource.Confidence == "High")
+                {
+                    Console.WriteLine(locationResource.Address.FormattedAddress);
+                }
+            }
+            Console.WriteLine();
+        }
+        public static void ProcessGeoCode(Response routesResponse)
+        {
+            // Get first location in the response and then extract the lat and long
+            Console.WriteLine("Getting Lat and Long");
+
+            var resouresSet = routesResponse.ResourceSets[0];
+            var location = (Location) resouresSet.Resources[0];
+
+            Console.WriteLine();
+
+            // Get the Geocode Points for each Location
+            for (var loc = 0; loc < locsCount; loc++)
+            {
+                var locationResource = (Location) resouresSet.Resources[loc];
+                Console.WriteLine("Geocode Points for " + locationResource.Address.FormattedAddress);
+
+                var countGeocodePoints = locationResource.GeocodePoints.Length;
+
+                for (var point = 0; point < countGeocodePoints; point++)
+                {
+                    var pointInfo = locationResource.GeocodePoints[point];
+
+                    Console.WriteLine($"    Point: {pointInfo.Coordinates[0]}, {pointInfo.Coordinates[1]}");
+
+                    Console.Write("    Usages: ");
+                    for (var usage = 0; usage < pointInfo.UsageTypes.Length; usage++)
+                    {
+                        Console.Write($"{pointInfo.UsageTypes[usage]} ");
+                    }
+                    Console.WriteLine("\n\n");
+                }
             }
             Console.WriteLine();
 
-            Console.WriteLine("Press any key to exit");
-            Console.ReadKey();
 
+            // Get all locations that have a Confidence=High
+            Console.WriteLine("Locations that have Confidence=High");
 
+            for (var loc = 0; loc < locsCount; loc++)
+            {
+                var locationResource = (Location)resouresSet.Resources[loc];
+
+                if (locationResource.Confidence == "High")
+                {
+                    Console.WriteLine(locationResource.Address.FormattedAddress);
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public static void ProcessRoutesResponse(Response routesResponse)
+        {
+            
         }
     }
 }
